98a049d6d57fcebef7fca95e2ca7c52474220abb
Víctor <victor.af1104@gmail.com>
2024-07-23 01:06:29 +0200

    Add HotelItemComponent for displaying hotel details

---
+++ src/app/features/hotels/components/hotel-item/hotel-item.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HotelItemComponent } from './hotel-item.component';
+
+describe('HotelItemComponent', () => {
+  let component: HotelItemComponent;
+  let fixture: ComponentFixture<HotelItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HotelItemComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(HotelItemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/hotels/components/hotel-item/hotel-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Hotel } from '../../models/hotel.model';
+
+@Component({
+  selector: 'app-hotel-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './hotel-item.component.html',
+})
+export class HotelItemComponent {
+  @Input() hotel!: Hotel;
+}
