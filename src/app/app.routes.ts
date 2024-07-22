019b8fd69e2661038f36e90856a70b5178203070
Víctor <victor.af1104@gmail.com>
2024-07-22 23:57:57 +0200

    Add routes for hotel list page and set default redirect path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'hoteles',
+    title: 'Listado de Hoteles',
+    loadComponent: () =>
+      import('./features/hotels/pages/hotel-list/hotel-list.component'),
+  },
+  {
+    path: '',
+    redirectTo: '/hoteles',
+    pathMatch: 'full',
+  },
+];
