1d5eb39e9df76e7f78347737d40332231a293e42
Víctor <victor.af1104@gmail.com>
2024-07-23 17:30:29 +0200

    Update HotelService to handle hotel recovery only

---
+++ src/app/features/hotels/services/hotel/hotel.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { catchError, Observable, of, tap } from 'rxjs';
+import { environment } from '@environments/environment';
+import { Hotel } from '@features/hotels/models/hotel.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class HotelService {
+  private readonly apiBaseUrl = environment.apiUrl;
+
+  hotels = signal<Hotel[]>([]);
+
+  constructor(private readonly http: HttpClient) {}
+
+  loadHotels(): Observable<Hotel[]> {
+    return this.http.get<Hotel[]>(`${this.apiBaseUrl}/hotels`).pipe(
+      catchError((error) => {
+        console.error('Error fetching hotels: ', error);
+
+        return of([]);
+      })
+    );
+  }
+}
