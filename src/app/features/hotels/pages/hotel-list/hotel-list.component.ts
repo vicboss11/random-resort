dc786d23cc45e87ca4c824cff56224ee6d2ebcec
Víctor <victor.af1104@gmail.com>
2024-07-23 19:47:59 +0200

    Update HotelListComponent to include pagination functionality

--- src/app/features/hotels/pages/hotel-list/hotel-list.component.ts
+++ src/app/features/hotels/pages/hotel-list/hotel-list.component.ts
@@ -4,15 +4,26 @@ import { HotelItemComponent } from '../../components/hotel-item/hotel-item.compo
 import { HotelFilterComponent } from '../../components/hotel-filter/hotel-filter.component';
 import { HotelService } from '../../services/hotel/hotel.service';
 import { HotelFilterService } from '../../services/hotel-filter/hotel-filter.service';
+import { PaginationComponent } from '@app/shared/components/pagination/pagination.component';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, HotelItemComponent, HotelFilterComponent],
+  imports: [
+    CommonModule,
+    HotelItemComponent,
+    HotelFilterComponent,
+    PaginationComponent,
+  ],
   templateUrl: './hotel-list.component.html',
+  styleUrls: ['./hotel-list.component.scss'],
 })
 export default class HotelListComponent implements OnInit {
   hotels = this.hotelService.hotels;
   filteredHotels = this.hotelFilterService.filteredHotels;
+
+  totalItems = this.hotelFilterService.totalItems;
+  currentPage = this.hotelFilterService.currentPage;
+  itemsPerPage = this.hotelFilterService.itemsPerPage;
 
   constructor(
     private readonly hotelService: HotelService,
@@ -23,6 +34,12 @@ export default class HotelListComponent implements OnInit {
     this.hotelService.loadHotels().subscribe((hotels) => {
       this.hotels.set(hotels);
       this.filteredHotels.set(hotels);
+
+      this.hotelFilterService.applyFilters();
     });
+  }
+
+  onPageChange(page: number): void {
+    this.hotelFilterService.setCurrentPage(page);
   }
 }
