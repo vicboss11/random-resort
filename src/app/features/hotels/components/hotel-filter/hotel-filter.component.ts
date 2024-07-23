d45b4630ab821b332366ddde60c580d341b9f37e
Víctor <victor.af1104@gmail.com>
2024-07-23 17:41:13 +0200

    Update HotelFilter component with new filter options and event handlers

--- src/app/features/hotels/components/hotel-filter/hotel-filter.component.ts
+++ src/app/features/hotels/components/hotel-filter/hotel-filter.component.ts
@@ -1,25 +1,45 @@
 import { Component } from '@angular/core';
+import { FilterCheckbox, FilterRange } from '@app/shared/models/filter.model';
+import { HotelFilterService } from '../../services/hotel-filter/hotel-filter.service';
+import {
+  filterPriceOptions,
+  filterRateOptions,
+  filterStarOptions,
+} from '../../constants/hotel-filter';
 import { FilterSearchComponent } from '@app/shared/components/filter-search/filter-search.component';
-import { HotelService } from '../../services/hotel.service';
+import { FilterRangeComponent } from '@app/shared/components/filter-range/filter-range.component';
+import { FilterCheckboxComponent } from '@app/shared/components/filter-checkbox/filter-checkbox.component';
 
 @Component({
   selector: 'app-hotel-filter',
   standalone: true,
-  imports: [FilterSearchComponent],
+  imports: [
+    FilterSearchComponent,
+    FilterCheckboxComponent,
+    FilterRangeComponent,
+  ],
   templateUrl: './hotel-filter.component.html',
 })
 export class HotelFilterComponent {
-  hotels = this.hotelService.getHotels();
-  filteredHotels = this.hotelService.getFilteredHotels();
+  filterStarCheckboxOptions: FilterCheckbox[] = filterStarOptions;
+  filterRateRangeOptions: FilterRange = filterRateOptions;
+  filterPriceRangeOptions: FilterRange = filterPriceOptions;
 
-  constructor(private readonly hotelService: HotelService) {}
+  constructor(private readonly hotelFilterService: HotelFilterService) {}
 
-  onSearchChange(searchTerm: string): void {
-    const term = searchTerm.toLowerCase();
-    const filteredHotels = this.hotels().filter((hotel) =>
-      hotel.name.toLowerCase().includes(term)
-    );
+  onSearchTermChange(searchTerm: string): void {
+    this.hotelFilterService.setSearchTerm(searchTerm);
+  }
 
-    this.filteredHotels.set(filteredHotels);
+  onStarOptionsChange(starsOptions: FilterCheckbox[]): void {
+    this.hotelFilterService.setSelectedStars(starsOptions);
+  }
+
+  onRateRangeChange(rateRange: number): void {
+    this.hotelFilterService.setRateRange(rateRange);
+  }
+
+  onPriceRangeChange(priceRange: number): void {
+    this.hotelFilterService.setPriceRange(priceRange);
   }
 }
