e5b6e218425c9e0ad766abd64e2998c99d9bc8c1
Víctor <victor.af1104@gmail.com>
2024-07-23 22:10:36 +0200

    Improve hotel item component layout and styling

--- src/app/features/hotels/components/hotel-item/hotel-item.component.ts
+++ src/app/features/hotels/components/hotel-item/hotel-item.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Hotel } from '@features/hotels/models/hotel.model';
+import { ScoreComponent } from '@app/shared/components/score/score.component';
+import { StarsComponent } from '@app/shared/components/stars/stars.component';
 
 @Component({
   selector: 'app-hotel-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, StarsComponent, ScoreComponent],
   templateUrl: './hotel-item.component.html',
+  styleUrls: ['./hotel-item.component.scss'],
 })
 export class HotelItemComponent {
   @Input() hotel!: Hotel;
