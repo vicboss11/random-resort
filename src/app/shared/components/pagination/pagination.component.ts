8495fb6654c1274fe1334d0e53b419f4e5860f23
Víctor <victor.af1104@gmail.com>
2024-07-23 19:46:52 +0200

    Add Pagination component

---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,40 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  standalone: true,
+  imports: [],
+  templateUrl: './pagination.component.html',
+})
+export class PaginationComponent {
+  @Input() currentPage = 1;
+  @Input() totalItems = 0;
+  @Input() itemsPerPage = 10;
+  @Output() pageChange: EventEmitter<number> = new EventEmitter();
+
+  get pages(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
+  }
+
+  get totalPages(): number {
+    return Math.ceil(this.totalItems / this.itemsPerPage);
+  }
+
+  onPageChange(page: number): void {
+    this.currentPage = page;
+
+    this.pageChange.emit(page);
+  }
+
+  previousPage(): void {
+    if (this.currentPage > 1) {
+      this.onPageChange(this.currentPage - 1);
+    }
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages) {
+      this.onPageChange(this.currentPage + 1);
+    }
+  }
+}
